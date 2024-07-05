4272450a1deecfdb5a05cb89db1cabcf11e08d26
Alexander Serraris <alexander.cs.dev@gmail.com>
2024-07-05 09:18:39 +0200

    College overzicht buttons die pagina functies aangeven

--- teacher_app/src/_pages/lecture_overview.py
+++ teacher_app/src/_pages/lecture_overview.py
@@ -1,24 +1,37 @@
 import streamlit as st
 from data.data_access_layer import DatabaseAccess
 
+
 class LectureOverview:
     def __init__(self):
         self.db_dal = DatabaseAccess()
-        st.session_state.lectures = [("1_Embryonale_ontwikkeling", "De ontwikkeling van een embryo van bevruchting tot geboorte en de invloed van externe factoren."),
-                ("2_Machine_Learning", "De fundamentele principes achter machine learning en hoe je die kunt implementern om bedrijfsprocessen te verbeteren."),
-                ("3_Data_analytics", "Hoe je data kunt analyseren en visualiseren om er waardevolle inzichten uit te halen en beslissingen te ondersteunen."),
-                ("4_Data_engineering", "Hoe je data pipelines kunt bouwen om data te verzamelen en te verwerkem van verschillende bronnen op grote schaal.")]
-    
-    
+        st.session_state.lectures = [
+            (
+                "1_Embryonale_ontwikkeling",
+                "De ontwikkeling van een embryo van bevruchting tot geboorte en de invloed van externe factoren.",
+            ),
+            (
+                "2_Machine_Learning",
+                "De fundamentele principes achter machine learning en hoe je die kunt implementern om bedrijfsprocessen te verbeteren.",
+            ),
+            (
+                "3_Data_analytics",
+                "Hoe je data kunt analyseren en visualiseren om er waardevolle inzichten uit te halen en beslissingen te ondersteunen.",
+            ),
+            (
+                "4_Data_engineering",
+                "Hoe je data pipelines kunt bouwen om data te verzamelen en te verwerkem van verschillende bronnen op grote schaal.",
+            ),
+        ]
+
     def render_logo(self):
-        st.image('src/data/content/images/logo.png', width=100)
+        st.image("src/data/content/images/logo.png", width=100)
 
     def render_sidebar(self):
         """
         Renders the contents of the sidebar, including the course buttons and login info.
         """
         with st.sidebar:
-
             image_col = st.columns([0.4, 1])
             with image_col[1]:
                 self.render_logo()
@@ -39,7 +52,7 @@ class LectureOverview:
             st.write("\n\n")
 
             # Login info & logout button
-            st.write(f"**Luc Mahieu**") #TODO: HARDCODED: replace with actual username
+            st.write(f"**Luc Mahieu**")  # TODO: HARDCODED: replace with actual username
             st.button("Uitloggen", use_container_width=True)
 
     def go_to_lecture(self, lecture_title):
@@ -52,22 +65,21 @@ class LectureOverview:
 
         print(f"Module status: {module_status}")
         print(f"Phase before: {st.session_state.selected_phase}")
-        if module_status == 'corrected':
-            st.session_state.selected_phase = 'insights'
-        elif module_status == 'generated':
-            st.session_state.selected_phase = 'quality_check'
-        elif module_status == 'not_recorded':
-            st.session_state.selected_phase = 'record'
-            
+        if module_status == "corrected":
+            st.session_state.selected_phase = "insights"
+        elif module_status == "generated":
+            st.session_state.selected_phase = "quality_check"
+        elif module_status == "not_recorded":
+            st.session_state.selected_phase = "record"
+
         print(f"Phase set to {st.session_state.selected_phase}")
 
         self.db_dal.update_last_module()
-        
 
     def render_page_title(self):
         title_html = open("./src/assets/html/lecture_title.html", "r").read()
         title_css = open("./src/assets/css/lecture_title.css", "r").read()
-        
+
         st.markdown(f"<style>{title_css}</style>", unsafe_allow_html=True)
         st.markdown(title_html, unsafe_allow_html=True)
 
@@ -78,28 +90,55 @@ class LectureOverview:
         for lecture_title, lecture_description in st.session_state.lectures:
             container = st.container(border=True)
             cols = container.columns([14, 6, 1])
-        
+
             with container:
                 # Render the button to view the lecture
                 with cols[1]:
                     st.write("\n\n")
-                    st.button("Leerstof bekijken",
-                              key=lecture_title,
-                              on_click=self.go_to_lecture,
-                              args=(lecture_title.replace('_', ' '),),
-                              use_container_width=True
-                    )
-                
+                    st.session_state.selected_module = lecture_title.replace("_", " ")
+                    module_status = self.db_dal.fetch_module_status()
+                    match module_status:
+                        case "corrected":
+                            st.button(
+                                "Inzichten bekijken",
+                                key=lecture_title,
+                                on_click=self.go_to_lecture,
+                                args=(lecture_title.replace("_", " "),),
+                                use_container_width=True,
+                            )
+                        case "generated":
+                            st.button(
+                                "Kwaliteitscontrole",
+                                key=lecture_title,
+                                on_click=self.go_to_lecture,
+                                args=(lecture_title.replace("_", " "),),
+                                use_container_width=True,
+                            )
+                        case "not_recorded":
+                            st.button(
+                                "Opnemen",
+                                key=lecture_title,
+                                on_click=self.go_to_lecture,
+                                args=(lecture_title.replace("_", " "),),
+                                use_container_width=True,
+                            )
+
+                    # st.button("Leerstof bekijken",
+                    #           key=lecture_title,
+                    #           on_click=self.go_to_lecture,
+                    #           args=(lecture_title.replace('_', ' '),),
+                    #           use_container_width=True
+                    # )
+
                 # Render the lecture title and description
                 with cols[0]:
-                    lecture_title = lecture_title.split('_', 1)[1].replace('_', ' ')
+                    lecture_title = lecture_title.split("_", 1)[1].replace("_", " ")
                     st.subheader(lecture_title)
                     st.write(lecture_description)
 
-
     def run(self):
         # Ensure this page is ran from the main controller and last visited page is displayed when user returns
-        self.db_dal.update_last_phase('lectures')
+        self.db_dal.update_last_phase("lectures")
 
         self.render_page_title()
 
@@ -110,6 +149,7 @@ class LectureOverview:
         # self.render_sidebar()
 
         self.render_page()
+
 
 if __name__ == "__main__":
     LectureOverview().run()
