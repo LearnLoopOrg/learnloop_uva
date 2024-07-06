2d5f558dd80336de1214f9e67a88a493ce342d52
Luc Mahieu <lucmauricemahieu@gmail.com>
2024-07-06 19:04:00 +0200

    fixed docker files for deployement

--- flask_app/main.py
+++ flask_app/main.py
@@ -30,29 +30,29 @@ surf_test_env = False
 db = db_config.connect_db(use_mongodb)
 
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET')
+app.secret_key = os.getenv("FLASK_SECRET")
 
 # Make authentication instance for the flask app
 auth = OAuth(app)
 
 auth.register(
-    name='surfconext',
-    client_id=os.getenv('SURFCONEXT_CLIENT_ID'),
-    client_secret=os.getenv('SURFCONEXT_CLIENT_SECRET'),
-    server_metadata_url=os.getenv('SURFCONEXT_METADATA_URL'),
-    client_kwargs={'scope': 'openid'}
+    name="surfconext",
+    client_id=os.getenv("SURFCONEXT_CLIENT_ID"),
+    client_secret=os.getenv("SURFCONEXT_CLIENT_SECRET"),
+    server_metadata_url=os.getenv("SURFCONEXT_METADATA_URL"),
+    client_kwargs={"scope": "openid"},
 )
 
 
-@app.route('/')
+@app.route("/")
 def login():
     global surf_test_env
     if surf_test_env:
-        scheme = 'http'
+        scheme = "http"
     else:
-        scheme = 'https'
-    
-    redirect_uri = url_for('authorize', _external=True, _scheme=scheme)
+        scheme = "https"
+
+    redirect_uri = url_for("authorize", _external=True, _scheme=scheme)
     return auth.surfconext.authorize_redirect(redirect_uri)
 
 
@@ -66,36 +66,36 @@ def save_id_to_db(user_id):
 def generate_nonce(length=16):
     """Generates a random sequence of values."""
     characters = string.ascii_letters + string.digits
-    nonce = ''.join(random.choice(characters) for _ in range(length))
+    nonce = "".join(random.choice(characters) for _ in range(length))
     return nonce
 
 
 def save_nonce_to_db(user_id):
     global db
     nonce = generate_nonce(16)
-    db.users_2.update_one({'username': user_id}, {'$set': {'nonce': nonce}})
+    db.users_2.update_one({"username": user_id}, {"$set": {"nonce": nonce}})
     return nonce
 
 
-@app.route('/auth')
+@app.route("/auth")
 def authorize():
     global surf_test_env
     token = auth.surfconext.authorize_access_token()
 
-    user_id = token['userinfo']['sub']
+    user_id = token["userinfo"]["sub"]
     save_id_to_db(user_id)
     nonce = save_nonce_to_db(user_id)
 
     # Redirect to streamlit app
     if surf_test_env:
-        url = 'http://localhost:8501/'
+        url = "http://localhost:8501/"
     else:
-        url = 'https://learnloop.datanose.nl/'
-    
-    redirect_url = f'{url}app?nonce={nonce}'
+        url = "https://learnloop.datanose.nl/"
+
+    redirect_url = f"{url}app?nonce={nonce}"
 
     return redirect(redirect_url, code=302)
 
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
