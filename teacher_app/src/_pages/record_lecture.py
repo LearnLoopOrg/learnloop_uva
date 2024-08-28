84887b222b66c688d7d4c74a7214df98a4638f15
mvanroessel <mvanroessel@tudelft.nl>
2024-08-28 11:19:04 +0200

    Removed use of pyaudio from record_lecture.py

--- teacher_app/src/_pages/record_lecture.py
+++ teacher_app/src/_pages/record_lecture.py
@@ -1,113 +1,114 @@
 from utils.utils import Utils
 import streamlit as st
-import numpy as np
-import cv2
-import mss
-import threading
-import wave
 import time
-import pyaudio
-import io
-import os
-import requests
 from dotenv import load_dotenv
 from data.data_access_layer import DatabaseAccess
+
+# import pyaudio
+# import io
+# import os
+# import requests
+# import numpy as np
+# import cv2
+# import mss
+# import threading
+# import wave
 
 load_dotenv()
 
 
 class Recorder:
     def __init__(self):
-        self.container_name_video = "video"
-        self.blob_name_video = "video_output.avi"
-        self.container_name_audio = "audio"
-        self.blob_name_audio = "video_output.mp3"
+        # self.container_name_video = "video"
+        # self.blob_name_video = "video_output.avi"
+        # self.container_name_audio = "audio"
+        # self.blob_name_audio = "video_output.mp3"
         self.utils = Utils()
         self.db_dal = DatabaseAccess()
 
-    def record_audio(self, duration):
-        format = pyaudio.paInt16
-        channels = 2
-        rate = 44100
-        chunk = duration * rate
+    # def record_audio(self, duration):
+    #     format = pyaudio.paInt16
+    #     channels = 2
+    #     rate = 44100
+    #     chunk = duration * rate
 
-        p = pyaudio.PyAudio()
+    #     p = pyaudio.PyAudio()
 
-        stream = p.open(
-            format=format,
-            channels=channels,
-            rate=rate,
-            input=True,
-            frames_per_buffer=chunk,
-        )
+    #     stream = p.open(
+    #         format=format,
+    #         channels=channels,
+    #         rate=rate,
+    #         input=True,
+    #         frames_per_buffer=chunk,
+    #     )
 
-        frames = []
+    #     frames = []
 
-        data = stream.read(chunk)
-        frames.append(data)
+    #     data = stream.read(chunk)
+    #     frames.append(data)
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    #     stream.stop_stream()
+    #     stream.close()
+    #     p.terminate()
 
-        audio_data = b"".join(frames)
-        audio_io = io.BytesIO()
-        wf = wave.open(audio_io, "wb")
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(format))
-        wf.setframerate(rate)
-        wf.writeframes(audio_data)
-        wf.close()
+    #     audio_data = b"".join(frames)
+    #     audio_io = io.BytesIO()
+    #     wf = wave.open(audio_io, "wb")
+    #     wf.setnchannels(channels)
+    #     wf.setsampwidth(p.get_sample_size(format))
+    #     wf.setframerate(rate)
+    #     wf.writeframes(audio_data)
+    #     wf.close()
 
-        # Seek to the beginning of the BytesIO object before reading it for upload
-        audio_io.seek(0)
-        self.utils.upload_content_to_blob_storage(
-            self.container_name_audio, self.blob_name_audio, audio_io
-        )
+    #     # Seek to the beginning of the BytesIO object before reading it for upload
+    #     audio_io.seek(0)
+    #     self.utils.upload_content_to_blob_storage(
+    #         self.container_name_audio, self.blob_name_audio, audio_io
+    #     )
 
-    def record_screen(self, duration, fps=15):
-        sct = mss.mss()
-        monitor = sct.monitors[1]
+    # def record_screen(self, duration, fps=15):
+    #     sct = mss.mss()
+    #     monitor = sct.monitors[1]
 
-        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    #     fourcc = cv2.VideoWriter_fourcc(*"XVID")
 
-        video_io = io.BytesIO()
+    #     video_io = io.BytesIO()
 
-        # Temporarily save to a local file (necessary for cv2.VideoWriter)
-        temp_filename = "temp_video.avi"
-        out = cv2.VideoWriter(
-            temp_filename, fourcc, fps, (monitor["width"], monitor["height"])
-        )
+    #     # Temporarily save to a local file (necessary for cv2.VideoWriter)
+    #     temp_filename = "temp_video.avi"
+    #     out = cv2.VideoWriter(
+    #         temp_filename, fourcc, fps, (monitor["width"], monitor["height"])
+    #     )
 
-        start_time = time.time()
-        while time.time() - start_time < duration:
-            img = np.array(sct.grab(monitor))
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-            out.write(img)
+    #     start_time = time.time()
+    #     while time.time() - start_time < duration:
+    #         img = np.array(sct.grab(monitor))
+    #         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    #         out.write(img)
 
-        out.release()
+    #     out.release()
 
-        self.utils.upload_file_to_blob_storage(
-            self.container_name_video, temp_filename, self.blob_name_video
-        )
-        # Optionally, clean up the temporary file
-        os.remove(temp_filename)
+    #     self.utils.upload_file_to_blob_storage(
+    #         self.container_name_video, temp_filename, self.blob_name_video
+    #     )
+    #     # Optionally, clean up the temporary file
+    #     os.remove(temp_filename)
 
-    def start_recording(self, duration):
-        # Start audio and screen recording in separate threads
-        audio_thread = threading.Thread(target=self.record_audio, args=(duration,))
-        screen_thread = threading.Thread(target=self.record_screen, args=(duration,))
+    # def start_recording(self, duration):
+    #     # Start audio and screen recording in separate threads
+    #     audio_thread = threading.Thread(target=self.record_audio, args=(duration,))
+    #     screen_thread = threading.Thread(target=self.record_screen, args=(duration,))
 
-        audio_thread.start()
-        screen_thread.start()
+    #     audio_thread.start()
+    #     screen_thread.start()
 
-        audio_thread.join()
-        screen_thread.join()
+    #     audio_thread.join()
+    #     screen_thread.join()
 
-        st.session_state["recording"] = False
+    #     st.session_state["recording"] = False
 
-    def stop_recording():
-        st.session_state["recording"] = False
+    # def stop_recording():
+    #     st.session_state["recording"] = False
 
     def run(self):
         self.db_dal.update_last_phase("record_lecture")
