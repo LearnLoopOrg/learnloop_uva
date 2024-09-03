b6d154795494fcc94efab2dc76f8771f860dbb39
mvanroessel <mvanroessel@tudelft.nl>
2024-09-03 17:37:25 +0200

    Let student_app make standard use of UvA variables to connect to CosmosDB

--- student_app/src/_pages/theory_overview.py
+++ student_app/src/_pages/theory_overview.py
@@ -7,7 +7,7 @@ from data.data_access_layer import DatabaseAccess
 
 class TheoryOverview:
     def __init__(self) -> None:
-        self.db = db_config.connect_db(st.session_state.use_mongodb)
+        self.db = db_config.connect_db(st.session_state.use_LL_cosmosdb)
         self.db_dal = DatabaseAccess()
         self.utils = Utils()
         self.image_handler = ImageHandler()
