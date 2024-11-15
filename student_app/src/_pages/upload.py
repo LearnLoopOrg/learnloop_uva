d65d4341d2cacc33657fc9d2f4c0e55a687a70e1
mvanroessel <mvanroessel@tudelft.nl>
2024-11-15 11:16:12 +0100

    Changed upload page to use post request.

--- student_app/src/_pages/upload.py
+++ student_app/src/_pages/upload.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from utils.utils import Utils
 from io import BytesIO
+import requests
+import time
 
 
 class UploadPage:
@@ -10,11 +12,39 @@ class UploadPage:
         self.title = "Creëer module"
         self.progress_bar = None
         self.progress_text = None
+        # self.api_url = "https://learnloopcontentpipeline.azurewebsites.net/api/contentpipelinehttp?code=8zG4ThuDDpzez8v46xyBvpR7iGWElTurc8Hzx4GgTKeyAzFumB3U7A=="
+        self.api_url = "http://localhost:7071/api/contentpipelinehttp"
 
     def progress_callback(self, bytes_transferred, total_bytes):
         progress = int(bytes_transferred / total_bytes * 100)
         self.progress_bar.progress(min(progress, 100))
         self.progress_text.write(f"{progress}% geüpload")
+
+    def trigger_content_pipeline(
+        self,
+        input_file_name: str,
+        module_name: str,
+        course_name: str,
+        output_language: str,
+    ):
+        try:
+            params = {
+                "input_file_name": input_file_name,
+                "module_name": module_name,
+                "course_name": course_name,
+                "output_language": output_language,
+            }
+
+            response = requests.get(self.api_url, params=params)
+
+            if response.status_code == 200:
+                return True, "Pipeline succesvol gestart"
+            else:
+                error_message = response.text
+                return False, f"Error bij het starten van de pipeline: {error_message}"
+
+        except requests.exceptions.RequestException as e:
+            return False, f"Connectie error: {str(e)}"
 
     def run(self):
         st.title(self.title)
@@ -24,7 +54,7 @@ class UploadPage:
             # Initialiseer de voortgangsbalk en statusbericht
             self.progress_bar = st.progress(0)
             self.progress_text = st.empty()
-
+            uploaded_file.name = uploaded_file.name.replace("MP4", "mp4")
             # Upload het bestand met de voortgangscallback
             self.utils.upload_content_to_blob_storage(
                 self.container_name,
@@ -34,39 +64,53 @@ class UploadPage:
             )
 
             st.success(
-                "Upload geslaagd! Geef een naam aan de module en klik op 'Genereer module' om de module te genereren."
+                f"Upload geslaagd van {uploaded_file.name}! Geef een naam aan de module en klik op 'Genereer module' om de module te genereren."
             )
 
             with st.form(key="generate_learning_path"):
                 module_name = st.text_input(
-                    # "Naam van module", value=uploaded_file.name, key="username"
                     "Naam van module",
-                    "...",
-                    key="username",
+                    key="module_name",
                 )
+                # Course name input
+                course_name = st.text_input(
+                    "Naam van cursus",
+                    "AI in Society",
+                    key="course_name",
+                )
+                output_language = st.selectbox(
+                    "Selecteer taal",
+                    options=["Nederlands", "English"],
+                    key="language",
+                )
+                output_language = (
+                    "dutch" if output_language == "Nederlands" else "english"
+                )
+
                 submit_button = st.form_submit_button("Genereer module")
-                # TODO: Implementeer de post request naar de backend om de module te genereren
+
                 if submit_button:
                     print(f"""
-                            Uitvoeren van post request naar backend om module te genereren met blob name: uploaded_materials/{uploaded_file.name} en module name: {module_name}.
+                            Uitvoeren van post request naar backend om module te genereren met blob name uploaded_materials/{uploaded_file.name} en module {module_name} in course {course_name} met output_language {output_language}.
                             """)
+                    # Wait for some time for upload to be set in blob storage
+                    # time.sleep(10)
+                    self.trigger_content_pipeline(
+                        input_file_name=uploaded_file.name,
+                        module_name=module_name,
+                        course_name=course_name,
+                        output_language=output_language,
+                    )
                     st.success(
                         f'Module \'{module_name}\' wordt gegenereerd; het leertraject zal binnen enkele minuten onder "Mijn vakken" > "Kies module"s verschijnen.'
                     )
 
-    ## Gebruik deze code om de post request te sturen naar de backend om de module te genereren.
-    # def show_spinner_till_generated(self):
-    #     with st.spinner("Oefenmaterialen worden gegenereerd..."):
-    #         url = "https://contentpipeline.azurewebsites.net/contentpipeline?code=YxHEt2ZBmN6YX912nsC_i9KVpof7RVlr3k1yMSmZXlajAzFu_xvH1w=="
-    #         params = {
-    #             "lecture": "celbio_3",
-    #             "run_full_pipeline": "true",
-    #             "upload_to_demo_db": "true",
-    #         }
-    #         print(f"Sending http request to: {url} with params: {params}")
-    #         # requests.get(url, params=params)
-    #         while st.session_state.generated is False:
-    #             status = self.db_dal.fetch_module_status()
-    #             if status == "generated":
-    #                 st.session_state.generated = True
-    #             time.sleep(1)  # Sleep to avoid overwhelming the database with requests
+
+if __name__ == "__main__":
+    upload_page = UploadPage()
+    upload_page.trigger_content_pipeline(
+        input_file_name="state_of_ai_report.mp4",
+        module_name="test",
+        course_name="test",
+        output_language="dutch",
+    )
